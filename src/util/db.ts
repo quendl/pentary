f653cd963c34b36046d95857a63799b5b6330fa9
vKxni <Konradtoesch@icloud.com>
2022-06-18 18:51:15 +0200

    added errors throws to db

--- src/util/db.ts
+++ src/util/db.ts
@@ -6,8 +6,12 @@ const myLogger = new Logger();
 import path from "path";
 require("dotenv").config({ path: path.resolve(__dirname, "../.././env") });
 
+if (!process.env.MONGODB_TOKEN)
+  throw new Error(`🔴 | No Database Connection string found in dotenv file.`);
+
 myLogger.info("Connecting to Database . . .");
-connect(process.env.MONGODB_TOKEN as string)
+
+connect(process.env.MONGODB_TOKEN)
   .then(() => {
     myLogger.success("Successfully connected to database.");
   })
