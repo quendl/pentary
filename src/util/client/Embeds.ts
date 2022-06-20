3682c228aec09edc075975629d1561b6490fcafa
vKxni <Konradtoesch@icloud.com>
2022-06-20 19:44:35 +0200

    added embed functions

---
+++ src/util/client/Embeds.ts
@@ -0,0 +1,25 @@
+import { MessageEmbed } from "discord.js";
+import emojis from "../frontend/emojis";
+
+export function Embed(
+  title: string,
+  description: string,
+  color: any,
+  footer: string,
+  author: string
+) {
+  return new MessageEmbed()
+    .setTitle(title)
+    .setDescription(description)
+    .setColor(color)
+    .setTimestamp()
+    .setFooter({ text: footer })
+    .setAuthor({ name: author });
+}
+
+export function permissionError(description: string) {
+  return new MessageEmbed()
+    .setDescription(`${emojis.error} | You are not allowed to do that!`)
+    .setColor("RED")
+    .setTimestamp();
+}
