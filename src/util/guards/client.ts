7a54740d0431f92b5aaaca6056cfe1925a1b75fa
vKxni <Konradtoesch@icloud.com>
2022-07-02 10:48:38 +0200

    added client init guard

---
+++ src/util/guards/client.ts
@@ -0,0 +1,17 @@
+import { CommandInteraction, MessageEmbed } from "discord.js";
+
+// database query
+import Guild from "../../models/Admin/ActivateClient";
+
+// config
+import emojis from "../../util/frontend/emojis";
+
+export async function clientCheck(interaction: CommandInteraction) {
+  const embed = new MessageEmbed()
+    .setDescription(`${emojis.error} | The client isn't initialized yet!`)
+    .setColor("RED");
+
+  const guildQuery = await Guild.findOne({ guildID: interaction.guild?.id });
+  if (!guildQuery)
+    return interaction.followUp({ embeds: [embed], ephemeral: true });
+}
