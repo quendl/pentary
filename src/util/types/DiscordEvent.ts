c54f1b6fd5c5b1634c1821f07ec5fc6fb8c37bf1
Konrad Koni <eventbeate@gmail.com>
2022-07-09 22:34:14 +0200

    reworked types

--- src/util/types/DiscordEvent.ts
+++
@@ -1,3 +0,0 @@
-import { ClientEvents } from "discord.js";
-
-export type DiscordEvents = keyof ClientEvents
--- src/util/types/Utils.ts
+++ src/util/types/Utils.ts
@@ -1,5 +1,10 @@
 import { ClientEvents } from "discord.js";
 
+export type DiscordEvents = keyof ClientEvents
+
+export type TypeOrPromise<Type> = Type | Promise<Type>;
+export type Permissions = "OWNER" | "USER";
+
 /**
  * Type the arguments of an event
  */
