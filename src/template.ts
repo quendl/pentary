94a7e351d08d4bafc8820ae5dfa679c457af6a05
Konrad Koni <eventbeate@gmail.com>
2022-07-15 13:02:52 +0200

    added cmd template

---
+++ src/template.ts
@@ -0,0 +1,12 @@
+import { SlashCommandBuilder } from "@discordjs/builders";
+import { CommandInteraction } from "discord.js";
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("")
+        .setDescription(""),
+    async execute(interaction: CommandInteraction) {
+
+
+    },
+};
