1325baafa4ae7cef7acf6c89704c3a2901d72998
Konrad Koni <eventbeate@gmail.com>
2022-08-12 21:09:36 +0200

    fixed cache bug

--- src/events/cron/ServerStatus.ts
+++ src/events/cron/ServerStatus.ts
@@ -2,11 +2,11 @@ import { TextChannel } from "discord.js";
 import cron from "node-cron";
 
 import client from "../../util/bot";
-const channel = client.channels.cache.get("985600742956072960") as TextChannel;
 
 import fetch from "cross-fetch";
 
 export async function ServerStatus() {
+    const channel = await client.channels.fetch ("985600742956072960") as TextChannel;
     // cron.schedule('0 0 * * *', () => {
     //     console.log('Running a job at midnight');
     // }, {
@@ -14,7 +14,7 @@ export async function ServerStatus() {
     //     timezone: "Germany/Berlin"
     // });
 
-    // every hour 21:00, 21:00
+    // every hour 21:00, 22:00
     cron.schedule('0 * * * *', () => {
         channel.send({ content: "Hello there :wave:" });
     });
